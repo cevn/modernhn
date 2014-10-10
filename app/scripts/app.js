9628dca3dd182a83505869449efa59b2c028fae2
Sameer Dhar <avielus@gmail.com>
2014-10-10 01:23:05 -0400

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name modernhnApp
+ * @description
+ * # modernhnApp
+ *
+ * Main module of the application.
+ */
+angular.module('modernhnApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils'
+  ]);
