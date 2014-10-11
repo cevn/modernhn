c0b01b17e10b69a9aa84318cf3ead951068ab8a3
Sameer Dhar <avielus@gmail.com>
2014-10-11 12:18:05 -0400

    Functioning prototype

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,25 @@
  * Controller of the modernhnApp
  */
 angular.module('modernhnApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebase, $http) {
+    var ref = new Firebase('https://hacker-news.firebaseio.com/v0/topstories');
+    
+    var limit = $firebase(ref.limit(10));
+
+    var topStoryRefs = limit.$asArray();
+
+    $scope.topStories = [];
+
+    topStoryRefs.$loaded().then(function (){
+      angular.forEach(topStoryRefs, function(value) {
+        $http.get('https://hacker-news.firebaseio.com/v0/item/'+value.$value+'.json').success(function (story){
+          if (story.type === 'story' && story.url !== undefined && story.url){
+            $scope.topStories.push(story);
+          }
+        });
+      });
+    });
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
