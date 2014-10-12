3269c93a602b71e7df3bef75e14d4a147c8ea81a
Sameer Dhar <avielus@gmail.com>
2014-10-12 07:23:37 -0400

    Finished first iteration of comments

---
+++ app/scripts/controllers/comments.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modernhnApp.controller:CommentsCtrl
+ * @description
+ * # CommentsCtrl
+ * Controller of the modernhnApp
+ */
+angular.module('modernhnApp')
+  .controller('CommentsCtrl', function ($scope, $routeParams, $firebase, $http, $sce) {
+    $scope.comments = [];
+
+    $http.get('https://hacker-news.firebaseio.com/v0/item/'+$routeParams.id+'.json').success(function (story){
+      if (story.type === 'story' && story.url !== undefined && story.url){
+        $scope.story = story;
+        angular.forEach(story.kids, function(id) {
+          $http.get('https://hacker-news.firebaseio.com/v0/item/'+id+'.json').success(function (comment){
+            if (comment.type === 'comment') {
+              $scope.comments.push(comment);
+            }
+          });
+        });
+      }
+    });
+    
+    $scope.to_trusted = function(html_code) {
+      return $sce.trustAsHtml(html_code);
+    }
+
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
