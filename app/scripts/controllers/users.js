e97d9f18cae1d24d80b5c0b984f39149f35d6162
Sameer Dhar <avielus@gmail.com>
2014-10-13 18:12:55 -0400

    initial scaffolding for users

---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name modernhnApp.controller:UsersCtrl
+ * @description
+ * # UsersCtrl
+ * Controller of the modernhnApp
+ */
+angular.module('modernhnApp')
+  .controller('UsersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
